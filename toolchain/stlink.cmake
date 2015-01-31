ca9827e1f26e31a8f38762a4818a89617e13399b
Alex Hodge <alex.hodge@gmail.com>
2015-01-31 20:48:51 +1300

    Fixed gdb target and switch to Geoffrey Brown start up script.

--- toolchain/stlink.cmake
+++ toolchain/stlink.cmake
@@ -1,5 +1,5 @@
 
 add_custom_target(flash DEPENDS ${CMAKE_PROJECT_NAME}.bin COMMAND st-flash write --reset ${CMAKE_PROJECT_NAME}.bin 0x8000000)
 add_custom_target(erase  COMMAND st-flash erase)
-add_custom_target(link COMMAND st-util :4242)
-#ADD_CUSTOM_TARGET(gdb DEPENDS ${CMAKE_PROJECT_NAME}.elf COMMAND arm-none-eabi-gdb --tui --se=${CMAKE_PROJECT_NAME}.elf --command=${CMAKE_CURRENT_LIST_DIR}/gdbcommands.txt)
+add_custom_target(link COMMAND st-util -1)
+add_custom_target(gdb DEPENDS ${CMAKE_PROJECT_NAME}.elf COMMAND arm-none-eabi-gdb --tui --se=${CMAKE_PROJECT_NAME}.elf --command=${CMAKE_CURRENT_LIST_DIR}/gdbcommands.txt)
